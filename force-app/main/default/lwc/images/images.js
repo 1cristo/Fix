735dd542776eb83053bca7a11fe71857e63cdb04
RodrigoJScript <rodrigo777.rjp@gmail.com>
2024-03-19 12:52:41 -0600

    carousel neuvo con slide automatico

--- force-app/main/default/lwc/images/images.js
+++ force-app/main/default/lwc/images/images.js
@@ -1,3 +1,17 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class Images extends LightningElement {}
+export default class Images extends LightningElement {
+    @track activeIndex = 0; // Inicializa el índice activo como 0
+
+    connectedCallback() {
+        // Configura un intervalo para cambiar el índice activo cada 2 segundos (2000 milisegundos)
+        setInterval(() => {
+            this.nextSlide();
+        }, 2000);
+    }
+
+    nextSlide() {
+        // Aumenta el índice activo para mostrar el siguiente slide
+        this.activeIndex = (this.activeIndex + 1) % 5; // Asume que tienes 5 slides, ajusta este número según sea necesario
+    }
+}
